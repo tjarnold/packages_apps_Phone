a6119173a8732850162b3ec5275cefc7b04ae5c5
Daniel <daxx4182@gmail.com>
2013-01-26 21:36:26 +0800

    XY Changes
    - 3G Tile
    - Full Screen ID

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -64,7 +64,9 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
     private static final int CMD_ANSWER_RINGING_CALL = 4;
     private static final int CMD_END_CALL = 5;  // not used yet
     private static final int CMD_SILENCE_RINGER = 6;
-    private static final int CMD_TOGGLE_LTE = 7; // not used yet
+    private static final int CMD_TOGGLE_LTE = 7;
+    private static final int CMD_TOGGLE_2G = 8;
+    private static final int CMD_TOGGLE_3G = 9;
 
     /** The singleton instance. */
     private static PhoneInterfaceManager sInstance;
@@ -317,6 +319,19 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
                 mMainThreadHandler.obtainMessage(CMD_TOGGLE_LTE));
         android.provider.Settings.Global.putInt(mApp.getContentResolver(),
                 android.provider.Settings.Global.PREFERRED_NETWORK_MODE, network);
+    }
+
+    public void toggle3G(boolean on) {
+        int network = -1;
+        if (on) {
+            network = PhoneConstants.NT_MODE_WCDMA_ONLY;
+        } else {
+            network = PhoneConstants.NT_MODE_WCDMA_PREF;
+        }
+        mPhone.setPreferredNetworkType(network,
+                mMainThreadHandler.obtainMessage(CMD_TOGGLE_3G));
+        Settings.Secure.putInt(mApp.getContentResolver(),
+                Settings.Global.PREFERRED_NETWORK_MODE, network);
     }
 
     private boolean showCallScreenInternal(boolean specifyInitialDialpadState,
